35cdf1c67b4916f292e702bf8806afbad0c59cc3
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-17 09:27:54 +0530

    filtered data to narrow down the search to find habitable planet

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > 0.36 
+    && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 
 // Reading CSV data as a stream --> returns raw buffers of bytes
 fs.createReadStream('kepler_data.csv')
@@ -10,12 +17,14 @@ fs.createReadStream('kepler_data.csv')
         columns: true
     }))
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (error) => {
         console.log(error);
     })
     .on('end', () => {
-        console.log(results);
-        console.log('Done!');
+        console.log(habitablePlanets.map(planet => planet.kepler_name));
+        console.log(`${habitablePlanets.length} habitable planets found!`);
     });
